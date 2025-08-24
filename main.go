8831f2fc31c4e763e1824cae206774e1b3520249
James Vongphasouk <james.vongpha@gmail.com>
2025-08-23 22:18:22 -0600

    prototype add and list functions

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
+// TODO: make use of JSON for storage
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
+
+type Task struct {
+	num int
+	name string
+	status string
+}
 func main() {
 	// use os.args to get the commands 
-	// TODO: list of commands (add "task desc", list, done [task number], del [task number])
+	// TODO: list of commands (add "task, ... ," , list, done [task number], del [task number])
 	
 	if len(os.Args) == 1 {
 		//print the default help thingy
@@ -19,6 +27,20 @@ func main() {
 
 	
 	switch command {
+	case "add":
+		task_names := os.Args[2:]
+		if len(task_names) == 0 {
+			fmt.Println("Not enough arguments. At least one task name is required to create a task after the ./todo command.")
+			return
+		}
+		add(task_names)
+		fmt.Println("Added task to tasks.txt!")
+	case "done":
+		task_numbers := os.Args[2:]
+		complete(task_numbers)
+	case "del":
+		task_numbers := os.Args[2:]
+		del(task_numbers)
 	case "list":
 		list()
 	default:
@@ -28,16 +50,68 @@ func main() {
 
 }
 
+// Given a slice of strings, add tasks with status of pending into tasks.json
+func add(tasks []string) {
+
+	// TODO: make new IDs from the tasks.json 
+	
+	var new_tasks []Task
+
+	for i, v := range tasks {
+		var new_task Task
+		new_task.name = v 
+		new_task.num = i + 1
+		new_task.status = "pending"
+
+		new_tasks = append(new_tasks, new_task)
+	}
+
+	// create a new file
+	file, err := os.Create("tasks.txt")
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
+	// write each new task to txt file 
+	for _, v := range new_tasks {
+		var status_box string
+		if v.status == "pending" {
+			status_box = "[ ]"
+		} else {
+			status_box = "[x]"
+		}
+		task_string :=  status_box + " " + v.name  + "\n"
+		file.WriteString(task_string)
+	}
+
+	file.Close()
+}
+
+// Given a slice of task numbers, change the status of each task to done 
+func complete(task_numbers []string) {
+
+}
+
+// Given a slice of task numbers, delete each task from the tasks.json
+func del(task_numbers []string) {
+
+}
+
 // Output the current task list to terminal 
 func list() {
-	// look for json file in current directory 
-	file, err := os.Open("tasks.json") 
+
+	file, err := os.Open("tasks.txt") 
 	if err != nil {
-		fmt.Printf("%s. Please create a task first with ./todo create", err)
+		fmt.Printf("%s. Please create a task first with ./todo add <task_1> <task_2> ... \n", err)
 		return 
 	}
 	
-	fmt.Println("Found file, tasks.json!")
+	scanner := bufio.NewScanner(file)
+
+	fmt.Println("Current list of tasks:")
+	for (scanner.Scan()) {
+		fmt.Println(scanner.Text())
+	}
 	file.Close()
 	
 }
