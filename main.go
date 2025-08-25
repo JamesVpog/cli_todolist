83f81da681d601a161172bc8bcf6401588a19a9e
James Vongphasouk <james.vongpha@gmail.com>
2025-08-25 00:04:21 -0600

    default help text

--- main.go
+++ main.go
@@ -7,25 +7,39 @@ import (
 	"os"
 )
 
+const default_help_text string = `./todo is a todolist manager cli written in Go.
+
+Usage:
+
+        ./todo <command> [arguments]
+
+The commands are:
+		add			create a new task 
+		done 		complete a task 
+		del			delete a task
+		list 		view all tasks 
+
+Use "./todo help <command>" for more information about a command.
+
+`
 
 type Task struct {
-	num int
-	name string
+	num    int
+	name   string
 	status string
 }
+
 func main() {
-	// use os.args to get the commands 
-	// TODO: list of commands (add "task, ... ," , list, done [task number], del [task number])
-	
+	// use os.args to get the commands
+	// TODO: list of commands (add task_1 task_2 ... , list, done [task number], del [task number])
+
 	if len(os.Args) == 1 {
-		//print the default help thingy
-		fmt.Println("TODO: default help thingy")
-		return 
+		fmt.Print(default_help_text)
+		os.Exit(1)
 	}
-	
+
 	command := os.Args[1]
 
-	
 	switch command {
 	case "add":
 		task_names := os.Args[2:]
@@ -44,8 +58,8 @@ func main() {
 	case "list":
 		list()
 	default:
-		fmt.Println("Command not recognized. Call ./todo --help for more information.")
-		return 
+		fmt.Print(default_help_text)
+		return
 	}
 
 }
@@ -53,13 +67,13 @@ func main() {
 // Given a slice of strings, add tasks with status of pending into tasks.json
 func add(tasks []string) {
 
-	// TODO: make new IDs from the tasks.json 
-	
+	// TODO: make new IDs from the tasks.json
+
 	var new_tasks []Task
 
 	for i, v := range tasks {
 		var new_task Task
-		new_task.name = v 
+		new_task.name = v
 		new_task.num = i + 1
 		new_task.status = "pending"
 
@@ -72,7 +86,7 @@ func add(tasks []string) {
 		fmt.Printf("%s", err)
 		return
 	}
-	// write each new task to txt file 
+	// write each new task to txt file
 	for _, v := range new_tasks {
 		var status_box string
 		if v.status == "pending" {
@@ -80,14 +94,14 @@ func add(tasks []string) {
 		} else {
 			status_box = "[x]"
 		}
-		task_string :=  status_box + " " + v.name  + "\n"
+		task_string := status_box + " " + v.name + "\n"
 		file.WriteString(task_string)
 	}
 
 	file.Close()
 }
 
-// Given a slice of task numbers, change the status of each task to done 
+// Given a slice of task numbers, change the status of each task to done
 func complete(task_numbers []string) {
 
 }
@@ -97,21 +111,21 @@ func del(task_numbers []string) {
 
 }
 
-// Output the current task list to terminal 
+// Output the tasks.json list to stdout
 func list() {
 
-	file, err := os.Open("tasks.txt") 
+	file, err := os.Open("tasks.json")
 	if err != nil {
 		fmt.Printf("%s. Please create a task first with ./todo add <task_1> <task_2> ... \n", err)
-		return 
+		return
 	}
-	
+
 	scanner := bufio.NewScanner(file)
 
 	fmt.Println("Current list of tasks:")
-	for (scanner.Scan()) {
+	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
 	file.Close()
-	
+
 }
